c9e86392aeb5bc80fcd138e7caaafa59c79ad7bf
Junaidzaheer <junaidzaheer@3786@gmail.com>
2017-08-10 09:02:01 -0700

    DONE

---
+++ AttendanceMark/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttendanceMark.Models;
+
+namespace WebApplication2.Controllers
+{
+
+   
+    public class HomeController : Controller
+    {
+       
+        StudentAttendanceEntities db = new StudentAttendanceEntities();
+        public ActionResult Index()
+        {
+            var listofstudents = db.Students.ToList();
+            ViewBag.studentslist = listofstudents;
+            return View();
+        }
+
+
+        
+        [HttpPost]
+        public ActionResult Create(Attendance markattendance, string markdate, int[] StudentId)
+        {
+
+            for (int i = 0; i < StudentId.Length; i++)
+            {
+                markattendance.SId = StudentId[i];
+                markattendance.Date = markdate;
+                db.Attendances.Add(markattendance);
+                db.SaveChanges();
+            }
+          
+            return Content("Attendance Marked..!!");
+        } 
+        
+       
+    }
+}
---
+++ AttendanceMark/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AttendanceMark.MvcApplication" Language="C#" %>
